142c7d53ebf42af53135625e98b93f877a83de16
Megan Fung <megan.fung@ringcentral.com>
2024-07-26 09:46:11 -0700

    add choose date

--- clean.py
+++ clean.py
@@ -47,6 +47,37 @@ def propagate_right(df):
     return df
 
 
+def choose_date(df):
+    """will make a decision for what date will populate a cell:
+    1. identify cells that are blank.
+    2. if a cell is blank find the cell that is not blank on its left and right.
+    3. identify the column date of the blank cell.
+    4. if the column date's month and year are prior to the agreement date in the left cell than populate the cell
+    with that agreement date
+    5. otherwise populate the cell with the agreement date in the right cell"""
+    dates = df.columns[5:]  # date columns start at column F
+    for index, row in df.iterrows():  # iterate through account ids
+        for date in range(len(dates) - 1):  # iterate through sale agreements for current account id
+            if pd.isna(row[dates[date]]):  # current cell is blank
+                if pd.notna(row[dates[date - 1]]):  # one cell to left is not blank
+                    left_date = pd.to_datetime(row[dates[date - 1]], errors='coerce')  # left cell should be populated
+                else:
+                    left_date = None
+                right_date = None
+                for right in range(date + 1, len(dates)):  # traverse cells to the right
+                    if pd.notna(row[dates[right]]):  # right cell is not blank
+                        right_date = pd.to_datetime(row[dates[right]], errors='coerce')  # assign date in this cell
+                        break
+                current_column_date = pd.to_datetime(dates[date], errors='coerce')  # blank cell's column date
+                if left_date is not None and right_date is not None:  # in between two cells that are populated
+                    # see if column date is prior to left agreement date
+                    if (current_column_date.month < left_date.month) and (current_column_date.year <= left_date.year):
+                        df.iloc[index, df.columns.get_loc(dates[date])] = left_date  # take left date
+                    else:
+                        df.iloc[index, df.columns.get_loc(dates[date])] = right_date  # take right date
+    return df
+
+
 def mark_done(df):
     """flag row as done if cells are populated properly:
     1. find the first cell populated in the row.
@@ -94,7 +125,8 @@ data = pd.read_excel(file_input)  # open and read excel
 
 # data = same_left_right_date(data)  # calling clean up
 # data = propagate_right(data)  # propagate dates to the right
-# data = fix_date_format(data)  # format dates
+data = choose_date(data)  # populate blank cells in between different agreement dates
+data = fix_date_format(data)  # format dates
 data = mark_done(data)
 
 output_file = 'cleaned.xlsx'  # write to this output file
